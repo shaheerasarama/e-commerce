466a047003e33b2883a59625952064ea9219159d
shaheerasarama <shaheerasarama@gmail.com>
2024-07-09 23:32:04 +0300

    createLoginPage

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Categories from "./Pages/Categories/Categories";
 import { Route, Routes } from "react-router-dom";
 import Product from "./Pages/Product/Product";
 import Products from "./Pages/Products/Products";
+import SignInSide from "./Pages/Login/Login";
 function App() {
   return (
     <ThemeProvider theme={theme}>
@@ -17,6 +18,7 @@ function App() {
           {/* <Route element={<>not Found!</>} path="/*"></Route> */}
           <Route element={<Product />} path="/:categorName/:id"></Route>
           <Route element={<Products />} path="/products"></Route>
+          <Route element={<SignInSide />} path="/login"></Route>
         </Routes>
         <Footer />
       </div>
